226b90384fdaed270807d395bc45fe0bd90d3ada
Nathaniel_428 <66856650+Nathaniel-428@users.noreply.github.com>
2020-06-15 02:16:52 -0400

    The Minecraft wiki exists you know.

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_attributelist.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_attributelist.java
@@ -7,14 +7,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.BOTH)
-@CommandParameters(description = "Lists all possible attributes.", usage = "/<command>")
+@CommandParameters(description = "Lists all possible attributes. You should probably use the Minecraft Wiki instead, lazy ass.", usage = "/<command>")
 public class Command_attributelist extends FreedomCommand
 {
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        String list = "All possible attributes: ";
+        String list = "Here's your attributes, next time use the minecraft wiki: ";
 
         for (Attribute attribute : Attribute.values())
         {
