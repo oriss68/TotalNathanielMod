ecc6fa6b634fb8d45587e8d6ce2c5924ded00b80
Nathaniel_428 <66856650+Nathaniel-428@users.noreply.github.com>
2020-06-15 22:04:36 -0400

    Cursed

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_curse.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_curse.java
@@ -10,13 +10,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
 @CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Curse someone - sends a cursed texture pack to the specified player.", usage = "/<command> <player>")
+@CommandParameters(description = "Time for another Minecraft cursed video", usage = "/<command> <player>")
 public class Command_curse extends FreedomCommand
 {
 
     /* The only problem with this is someone can prevent themself from being cursed by declining to download the
        resource pack. However, if they hit yes, then you can curse them whenever you want and they can't stop it unless
        they go into their server settings. and set server resource packs to prompt or disabled */
+    // TODO: Kick players if they don't accept the pack.
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
