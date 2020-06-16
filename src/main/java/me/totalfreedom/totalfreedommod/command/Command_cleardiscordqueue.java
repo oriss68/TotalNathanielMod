41f5e195f83ba5d8c556e4d3500209d036121754
Nathaniel_428 <66856650+Nathaniel-428@users.noreply.github.com>
2020-06-15 21:49:21 -0400

    I let you down, again...

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_cleardiscordqueue.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_cleardiscordqueue.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.ONLY_CONSOLE)
-@CommandParameters(description = "Clear the discord message queue.", usage = "/<command>")
+@CommandParameters(description = "Does some Discord queue stuff", usage = "/<command>")
 public class Command_cleardiscordqueue extends FreedomCommand
 {
 
@@ -14,7 +14,7 @@ public class Command_cleardiscordqueue extends FreedomCommand
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         plugin.dc.clearQueue();
-        msg("Cleared the discord message queue.");
+        msg("Cleared the Discord queue, I don't have a cool joke for this");
         return true;
     }
 }
