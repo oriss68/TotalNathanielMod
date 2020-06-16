abfd31a979da23f5688cf38ed2af3f4f907111a1
Nathaniel_428 <66856650+Nathaniel-428@users.noreply.github.com>
2020-06-15 22:00:54 -0400

    "COOKIE" - Cookie Monster

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_cookie.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_cookie.java
@@ -15,7 +15,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 @CommandParameters(description = "For those who have no friends - gives a cookie to everyone on the server.", usage = "/<command>")
 public class Command_cookie extends FreedomCommand
 {
-    public static final String COOKIE_LYRICS = "Imagine that you have zero cookies and you split them evenly among zero friends. How many cookies does each person get? See? It doesn't make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends.";
+    public static final String COOKIE_LYRICS = "Imagine that you have zero cookies and you split them evenly among zero friends. How many cookies does each person get? See? It doesn't make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends. And plus, you created a black hole!";
     public static final String LORE = "But, you can have a cookie anyways,\nsince you are sad you are have no friends.";
 
     @Override
